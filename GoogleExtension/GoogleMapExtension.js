a0fcb4c4d1e3873b991181f5a78990bcdb191ca5
Fredy <FredyFadel@users.noreply.github.com>
2024-05-05 09:12:27 +0200

    PlaceInput

--- GoogleExtension/GoogleMapExtension.js
+++ GoogleExtension/GoogleMapExtension.js
@@ -7,9 +7,8 @@ Aspectize.Extend("GoogleMapPlaceInput", {
 
         var options = null;
         var autocomplete = null;
-        var listener = null;
 
-        function palceChanged() {
+        function placeChanged() {
             var place = autocomplete.getPlace();
             if (!place.geometry) {
                 return;
@@ -59,88 +58,31 @@ Aspectize.Extend("GoogleMapPlaceInput", {
             if (searchType || rectrictedCountry) {
 
                 options = {};
-                if (searchType) {
+                if (searchType) options.types = searchType.split(',');
+                if (rectrictedCountry) options.componentRestrictions = { country: rectrictedCountry };
+            }
 
-                    options.types = searchType.split(',');
-                }
+            if (autocomplete) google.maps.event.clearInstanceListeners(autocomplete);
 
-                if (rectrictedCountry) {
-
-                    options.componentRestrictions = { country: rectrictedCountry };
-                }
-
-                if (listener) google.maps.event.removeListener(listener);
-
-                var pacContainer = document.querySelector('.pac-container');
-                if (pacContainer) {
-                    pacContainer.parentNode.removeChild(pacContainer);
-                }
-
+            var pacContainer = document.querySelector('.pac-container');
+            if (pacContainer) {
+                pacContainer.parentNode.removeChild(pacContainer);
             }
 
             autocomplete = options ? new google.maps.places.Autocomplete(elem, options) : new google.maps.places.Autocomplete(elem);
-
-            listener = google.maps.event.addListener(autocomplete, 'place_changed', palceChanged);
+            google.maps.event.addListener(autocomplete, 'place_changed', placeChanged);
         }
 
         rebuildAutocomplete();
 
-        //#region ToDelete
-        //var autocomplete = new google.maps.places.Autocomplete(elem);
-
-        //google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        //    var place = autocomplete.getPlace();
-        //    if (!place.geometry) {
-        //        return;
-        //    }
-
-        //    if (place.geometry.location) {
-        //        Aspectize.UiExtensions.ChangeProperty(elem, 'Longitude', place.geometry.location.lng());
-        //        Aspectize.UiExtensions.ChangeProperty(elem, 'Latitude', place.geometry.location.lat());
-        //    }
-
-        //    var address = '';
-        //    if (place.address_components) {
-
-        //        for (var i = 0; i < place.address_components.length; i++) {
-        //            var adressElement = place.address_components[i];
-
-        //            for (var j = 0; j < adressElement.types.length; j++) {
-        //                var type = adressElement.types[j];
-        //                var property;
-        //                if (type === "postal_code") {
-        //                    property = "Zip";
-        //                } else if (type == "country") {
-        //                    property = "Country";
-        //                } else if (type == "locality") { // "locality") {
-        //                    property = "City";
-        //                } else if (type == "street_number") {
-        //                    property = "StreetNumber";
-        //                } else if (type == "route") {
-        //                    property = "Route";
-        //                } else continue;
-
-        //                value = adressElement.long_name;
-        //                Aspectize.UiExtensions.ChangeProperty(elem, property, value);
-        //                break;
-        //            }
-        //        }
-        //    }
-        //    Aspectize.UiExtensions.ChangeProperty(elem, 'Name', place.name);
-        //    Aspectize.UiExtensions.ChangeProperty(elem, 'FullAdress', place.formatted_address);
-        //});
-        //#endregion
-
         Aspectize.UiExtensions.AddMergedPropertyChangeObserver(elem, function (sender, arg) {
+
             if ('FullAdress' in arg) {
                 $(elem).val(arg.FullAdress);
             }
 
-            if (arg.SearchType || arg.RestrictToCountry) {
-
-                rebuildAutocomplete();
-            }
-
+            if (arg.SearchType)   rebuildAutocomplete();
+            if (arg.RestrictToCountry) autocomplete.setComponentRestrictions({ country: arg.RestrictToCountry });
         });
     }
 });
